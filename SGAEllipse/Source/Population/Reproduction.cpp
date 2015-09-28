aa851933318863d1e82d2e1233753fe6da027c2e
Suui <suui.gd@gmail.com>
2015-09-28 02:39:11 +0200

    Refactor

--- SGAEllipse/Source/Population/Reproduction.cpp
+++ SGAEllipse/Source/Population/Reproduction.cpp
@@ -1,5 +1,4 @@
 ﻿#include "Reproduction.h"
-#include "Individual.h"
 
 
 std::vector<int> Reproduction::CrossoverBetween(std::vector<int> firstParent, std::vector<int> secondParent, int crossoverPoint)
@@ -16,20 +15,20 @@ std::vector<Individual> Reproduction::Reproduce(Individual father, Individual mo
 	std::vector<Property> fatherProps = father.Properties();
 	std::vector<Property> motherProps = mother.Properties();
 
-	std::vector<std::vector<int>> sons;
+	std::vector<std::vector<int>> firstSon, secondSon;
 	int crossoverPoint;
 
 	for (unsigned int i = 0, size = fatherProps.size(); i < size; i++)
 	{
 		crossoverPoint = Utility::IntegerBetween(0, fatherProps[i].Binary().size());
 
-		sons.push_back(CrossoverBetween(fatherProps[i].Binary(), motherProps[i].Binary(), crossoverPoint));
-		sons.push_back(CrossoverBetween(motherProps[i].Binary(), fatherProps[i].Binary(), crossoverPoint));
+		firstSon.push_back(CrossoverBetween(fatherProps[i].Binary(), motherProps[i].Binary(), crossoverPoint));
+		secondSon.push_back(CrossoverBetween(motherProps[i].Binary(), fatherProps[i].Binary(), crossoverPoint));
 	}
 
 	std::vector<Individual> descendance;
-	descendance.push_back(Individual(sons[0], sons[2], sons[4], sons[6], sons[8]));
-	descendance.push_back(Individual(sons[1], sons[3], sons[5], sons[7], sons[9]));
+	descendance.push_back(Individual(firstSon[0], firstSon[1], firstSon[2], firstSon[3], firstSon[4]));
+	descendance.push_back(Individual(secondSon[0], secondSon[1], secondSon[2], secondSon[3], secondSon[4]));
 
 	return descendance;
 }
--- SGAEllipse/Source/Population/Reproduction.h
+++ SGAEllipse/Source/Population/Reproduction.h
@@ -1,7 +1,5 @@
 ﻿#pragma once
-#include <vector>
-
-class Individual;
+#include "Individual.h"
 
 
 class Reproduction
